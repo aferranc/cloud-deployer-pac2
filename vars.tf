4d90c3656e45b4aef8daa1c822f357b667fcf15e
Albert Ferran Casas <albert.ferran@gmail.com>
2022-07-02 06:47:16 +0000

    remove EC2_USER

--- vars.tf
+++ vars.tf
@@ -2,10 +2,6 @@ variable "AWS_REGION" {
     default = "eu-central-1"
 }
 
-variable "EC2_USER" {
-    default = "ec2-user"
-}
-
 variable "PRIVATE_KEY_PATH" {
   default = "AFERRANC-CCF-Key-WebServer"
 }
